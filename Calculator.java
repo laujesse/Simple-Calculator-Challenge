972e7ea8ec377f7ac0e60ad6b667d9c9968c5385
laujesse <170717@hkis.edu.hk>
2016-08-23 14:36:06 +0800

    Add files via upload

--- Calculator.java
+++ Calculator.java
@@ -6,31 +6,39 @@
  * Version 1 
  */
 import java.util.Scanner;
-public class Calculator {
-    public static void main(String[] args)  {
+public class Calculator 
+{
+    public static void main(String[] args)  
+    {
         Scanner Calculator = new Scanner(System.in);
-        double NumberOne, NumberTwo, Operator, Result;
+        double NumberOne, NumberTwo, Result;
+        String Operator;
         
         System.out.println("Enter the first number: ");
         NumberOne = Calculator.nextDouble();
+        System.out.println("Enter one of the following operators: +, -, *, /");
+        Operator = Calculator.next();
         System.out.println("Enter the second number: ");
         NumberTwo = Calculator.nextDouble();
-        System.out.println("Enter 1 for addition, Enter 2 for subtraction" + "\n" + "Enter 3 for multiplication, Enter 4 for division");
-        Operator = Calculator.nextDouble();
         
-        if (Operator == 1)  {
+        
+        if (Operator.equals("+"))  
+        {
             Result = NumberOne + NumberTwo;
             System.out.println(Result);
         }
-        if (Operator == 2)  {
+        if (Operator.equals("-"))  
+        {
             Result = NumberOne - NumberTwo;
             System.out.println(Result);
         }
-        if (Operator == 3)  {
+        if (Operator.equals("*"))  
+        {
             Result = NumberOne * NumberTwo;
             System.out.println(Result);
         }
-        if (Operator == 4)  {
+        if (Operator.equals("/"))  
+        {
             Result = NumberOne / NumberTwo;
             System.out.println(Result);
         }
